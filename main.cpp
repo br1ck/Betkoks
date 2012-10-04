0dd286261adab33e90d9780bf218a3fe09398e91
Robertas Smaliukas <saturnas@gmail.com>
2012-10-04 10:39:29 +0200

    nauja eilute

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "Press the enter key to continue ..." << endl;
+    cout << "dfg";
+    cin.get();
+    return EXIT_SUCCESS;
+}
